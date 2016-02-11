e9737d1c25cd2513b15ec67e24b401868f77d3e9
TheDanielBrain <danielbrainhh@hotmail.com>
2016-02-11 11:57:53 -0600

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -5,17 +5,12 @@ define('RUTA', 'http://localhost/fatbullets');
 $bd_config = array(
 	'basedatos' => 'fatbullets',
 	'usuario' => 'root',
-	'pass' => 'Bones1000-'
+	'pass' => ''
 );
 
 $blog_config = array(
 	'post_por_pagina' => '3',
 	'carpeta_imagenes' => 'img/posts/'
-);
-
-$blog_admin = array(
-	'usuario' => 'daniel',
-	'password' => '123'
 );
 
 ?>
